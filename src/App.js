6120dde06684b8968900e5ed790e9611ef20fe03
jslworks <jslworksprojects>
2022-02-09 03:28:22 +0100

    Show init state and is updated 1 when a button is clicked

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  // imgs contiene el estado actual. setImages permitirá la actualización del estado del Comp
+  const [imgs, setImgs] = useState([IMAGENES]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className='App-content'>
+        {
+          imgs.map(singleImg => <img src={singleImg} alt= 'horse' />)
+        }
+        <button onClick={()=> setImgs(IMAGENES2)}>
+          Cambiar Imgs
+        </button>
+      </section>
     </div>
   );
 }
+
+// Imagenes
+const endURL = '?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YW5kYWx1c2lhbiUyMGhvcnNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=700&q=60';
+
+const IMAGENES = [
+  'https://images.unsplash.com/photo-1553284965-e2815db2e5a0' + endURL,
+  'https://images.unsplash.com/photo-1553284965-83fd3e82fa5a' + endURL
+];
+const IMAGENES2 = [
+  'https://images.unsplash.com/photo-1504310977373-186d29f99322' + endURL,
+  'https://images.unsplash.com/photo-1606107869722-d5cbadabe2f0' + endURL
+];
 
 export default App;
