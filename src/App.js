132ec4272ec5f55e558b941e507fce2aa9dbe533
jslworks <jslworksprojects>
2022-02-10 01:58:26 +0100

    1st Component & put unique key for mapping func

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import getGifs from "./services/getGifs";
+import Gif from './components/Gifs'
 
 export default function App() {
   // imgs contiene el estado actual. setImages permitirá la actualización del estado del Comp
@@ -11,25 +12,23 @@ export default function App() {
       Si dejas un [], se ejecutará la 1a vez y ya está */
   useEffect(() => {
     console.log("actualizando gifs");
-    
+
     /* Exportamos la lógica de servicio para obtener los gifs
        y al obtener respuesta, actualizamos nuestro estado con los gifs recibidos */
-    getGifs({keyword : 'horse' }).then((gifs) => setGifs(gifs));        
+    getGifs({ keyword: "horse" }).then((gifs) => setGifs(gifs));
   }, []);
 
   return (
     <div className="App">
       <section className="App-content">
-        {
-          gifs.map((singleGif) => {
-            return <div>
-                <h4>{singleGif.title}</h4>
-                <small>{singleGif.id}</small>
-                <img src={singleGif.url} alt={singleGif.title} />
-              </div>
-            
-          })
-        }
+        {gifs.map((singleGif) => (
+          <Gif
+            key={singleGif.id}
+            id={singleGif.id}
+            title={singleGif.title}
+            url={singleGif.url}
+          />
+        ))}
       </section>
     </div>
   );
