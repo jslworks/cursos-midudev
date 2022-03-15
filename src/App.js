adfa4829b54089d8a11ad060146f36310217ec85
jslworks <jslworksprojects>
2022-03-15 19:58:16 +0100

    Make  ListOfGifs Component and free code in App

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
-import { useEffect, useState } from "react";
 import "./App.css";
-import getGifs from "./services/getGifs";
-import Gif from './components/Gifs'
+import ListOfGifs from "./components/ListOfGifs";
 
 export default function App() {
-  // imgs contiene el estado actual. setImages permitirá la actualización del estado del Comp
-  const [gifs, setGifs] = useState([]);
-
-  // Cada vez que se renderice el componente, useEffect actuará
-  /* El primer parámetro es el cambio, el 2º las dependencias o casos en los cuales cambiará
-      Si dejas un [], se ejecutará la 1a vez y ya está */
-  useEffect(() => {
-    console.log("actualizando gifs");
-
-    /* Exportamos la lógica de servicio para obtener los gifs
-       y al obtener respuesta, actualizamos nuestro estado con los gifs recibidos */
-    getGifs({ keyword: "horse" }).then((gifs) => setGifs(gifs));
-  }, []);
-
+  
   return (
     <div className="App">
       <section className="App-content">
-        {gifs.map((singleGif) => (
-          <Gif
-            key={singleGif.id}
-            title={singleGif.title}
-            url={singleGif.url}
-          />
-        ))}
+        <ListOfGifs keyword={"chile"} />
       </section>
     </div>
   );
---
+++ src/components/ListOfGifs.js
@@ -0,0 +1,29 @@
+import { react, useEffect, useState } from "react";
+import getGifs from "../services/getGifs";
+import Gif from './Gif';
+
+export default function ListOfGifs({ keyword }) {
+    // imgs contiene el estado actual. setImages permitirá la actualización del estado del Comp
+    const [gifs, setGifs] = useState([]);
+
+    // Cada vez que se renderice el componente, useEffect actuará
+    /* El primer parámetro es el cambio, el 2º las dependencias o casos en los cuales cambiará
+        Si dejas un [], se ejecutará la 1a vez y ya está */
+    useEffect(() => {
+        console.log("actualizando gifs");
+
+        /* Exportamos la lógica de servicio para obtener los gifs
+        y al obtener respuesta, actualizamos nuestro estado con los gifs recibidos */
+        getGifs({ keyword })
+            .then((gifs) => setGifs(gifs));
+    }, []);
+
+    return gifs.map(({ id, title, url }) => 
+        <Gif
+            id={id}
+            key={id}
+            title={title}
+            url={url}
+        />
+    )
+}
